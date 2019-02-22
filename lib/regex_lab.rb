69dd52151afd69a2c35f982774a35f44a844607b
Tracy Ngo <tngo1206@gmail.com>
2019-02-22 17:28:32 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,31 @@
 def starts_with_a_vowel?(word)
-
+ if word.match(/^[aeiouAEIOU]/)
+    true
+  else
+    false
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/un\w*ing/)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b\w{5,5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  if text.match(/^[A-Z].+[[:punct:]]$/)
+    true
+  else 
+    false
+  end
 end
 
 def valid_phone_number?(phone)
-
+  if phone.match(/(\d ?){10}|(\(\d{3}\)((\d{3}-\d{4})|\d{7})\b)/)
+    true
+  else 
+    false
+  end
 end
